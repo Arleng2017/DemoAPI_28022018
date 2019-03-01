5750aa41381a0944a6bbfb20498f83a02cc58387
Sakul Jaruthanaset <au.perspectives@gmail.com>
2019-03-01 23:59:03 +0700

    create nameController

---
+++ DemoAPI/Controllers/NameController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DemoAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NameController : ControllerBase
+    {
+        public static List<string> allMember = new List<string>
+         { "Name :Mickey", "Name :Dorarmon" };
+
+        [HttpGet]
+        public List<string> Get()
+        {
+            return allMember;
+        }
+
+        [HttpGet("{GetByIndex}")]
+        public string GetById(int GetByIndex)
+        {
+            if (allMember[GetByIndex] != null)
+            {
+                return allMember[GetByIndex];
+            }
+            return "ตำแหน่งที่ส่งมาไม่ถูกต้อง";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string name)
+        {
+            allMember.Add(name);
+        }
+
+        // PUT api/values/5
+        [HttpPut("{index}")]
+        public void Put(int index, [FromBody] string name)
+        {
+            if (allMember[index] != null)
+            {
+                allMember[index] = name;
+            }
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{index}")]
+        public void Delete(int index)
+        {
+            var deleteName = allMember[index];
+            allMember.Remove(deleteName);
+        }
+    }
+}
